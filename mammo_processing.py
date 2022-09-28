2f70533dd77945a28741dfebf58e8e8d1516ac1e
Juan Pablo Tavacchi <jptavac@gmail.com>
2022-09-28 10:51:35 -0300

    Update mammo_processing.py

--- mammo_processing.py
+++ mammo_processing.py
@@ -149,13 +149,13 @@ def largest_contour_segmenting_based_in_pixel_luminancy_threshold(image_array, b
     #Binarize image for contour recognition.
     image_bin = binarize_with_threshold(image_dark_sides, bin_threshold, normalized)   
     # Identify binarized image contours.
-    [contours, contour_hierarchy] = cv2.findContours(np.array(image_bin), mode=cv2.RETR_CCOMP , method=cv2.CHAIN_APPROX_NONE)  
+    [contours, contour_hierarchy] = cv2.findContours(image_bin, mode=cv2.RETR_CCOMP , method=cv2.CHAIN_APPROX_NONE)  
     # Get the largest contour index.
     largest_contour_index = get_largest_contour_index(contours)
     # Generate a black base image for contour with the same shape as the passed image.
     largest_contour_mask = generate_black_background(image_dark_sides)  
     # Generate a binary mask based on the largest contour.
-    cv2.drawContours(image=largest_contour_mask.tolist(), contours=[contours[largest_contour_index]], contourIdx=-1, color=(255), thickness=-1)
+    cv2.drawContours(image=largest_contour_mask, contours=[contours[largest_contour_index]], contourIdx=-1, color=(255), thickness=-1)
     # Obtain segmented image.
     image_segmented = apply_mask(image_array, largest_contour_mask)
 
